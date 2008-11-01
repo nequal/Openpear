cc432488f8beaa04b72eb460d3ce5ac309701f45
riaf <riaf@d0558523-a964-43cb-96e9-15da18736084>
2008-11-01 06:00:31 +0000

    おちゃめ
    git-svn-id: http://svn.nequal.jp/websites/openpear.org/trunk@357 d0558523-a964-43cb-96e9-15da18736084

--- library/OpenpearFormatter.php
+++ library/OpenpearFormatter.php
@@ -1,4 +1,6 @@
 <?php
+Rhaco::import('lang.ArrayUtil');
+Rhaco::import('tag.model.TemplateFormatter');
 Rhaco::import('HatenaSyntax');
 
 class OpenpearFormatter
@@ -28,5 +30,23 @@ class OpenpearFormatter
     function pp($str){
         list(, $n) = explode('/', $str);
         return $n;
+    }
+    function gravatar($mail, $opt=''){
+        $option = array(
+            's' => 16,
+            'r' => 'R',
+        );
+        if(!empty($opt)){
+            $params = ArrayUtil::dict($opt, array('d', 's', 'r'), false);
+            $option = array_merge($option, $params);
+        }
+        if(!isset($option['d'])) $option['d'] = 'http://openpear.org/icons/?s='. $option['s'];
+
+        $url = 'http://www.gravatar.com/avatar/'. md5($mail). '?'.
+            TemplateFormatter::httpBuildQuery($option);
+        return sprintf('<img src="%s" width="%s" height="%s" alt="gravatar" />',
+            $url,
+            $option['s'], $option['s']
+        );
     }
 }
