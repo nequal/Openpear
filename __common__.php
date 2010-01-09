285c77d1d8d940b2570497ffe61aca3bccc86325
Keisuke SATO <riafweb@gmail.com>
2010-01-10 02:32:27 +0900

    autoload

---
+++ __common__.php
@@ -0,0 +1,36 @@
+<?php
+
+function openpear_autoload_handler($class_name){
+    $modules = array('Account', 'Document', 'Maintainer',
+        'Message', 'Openpear', 'Package', 'Release', 'Source', 'Timeline');
+    if(isset($modules[$class_name])){
+        import('org.openpear.'. $modules[$class_name]);
+    }
+    $models = array(
+        // models
+        'OpenpearMaintainer' => 'Maintainer.model.OpenpearMaintainer',
+        'OpenpearOpenidMaintainer' => 'Maintainer.model.OpenpearOpenidMaintainer',
+        'OpenpearMessage' => 'Message.model.OpenpearMessage',
+        'OpenpearCharge' => 'Package.model.OpenpearCharge',
+        'OpenpearFavorite' => 'Package.model.OpenpearFavorite',
+        'OpenpearNewprojectQueue' => 'Package.model.OpenpearNewprojectQueue',
+        'OpenpearPackage' => 'Package.model.OpenpearPackage',
+        'OpenpearPackageMessage' => 'Package.model.OpenpearPackageMessage',
+        'OpenpearPackageTag' => 'Package.model.OpenpearPackageTag',
+        'OpenpearTag' => 'Package.model.OpenpearTag',
+        'OpenpearRelease' => 'Release.model.OpenpearRelease',
+        'OpenpearReleaseQueue' => 'Release.model.OpenpearReleaseQueue',
+        'OpenpearChangeset' => 'Source.model.OpenpearChangeset',
+        'OpenpearTimeline' => 'Timeline.model.OpenpearTimeline',
+        // others
+        'OpenpearFlow' => 'Openpear.OpenpearFlow',
+        'OpenpearTemplf' => 'Openpear.OpenpearTemplf',
+        'OpenpearAccountModule' => 'Openpear.module.OpenpearAccountModule',
+        'OpenpearException' => 'Openpear.exception.OpenpearException',
+    );
+    if(isset($models[$class_name])){
+        import('org.openpear.'. $models[$class_name]);
+    }
+}
+
+spl_autoload_register('openpear_autoload_handler');
