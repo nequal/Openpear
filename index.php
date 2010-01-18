95aeba626ad5a286c5e7cdf1bedebef67362ca97
tokushima <rhaco1004@gmail.com>
2010-01-19 00:36:40 +0900

    改行いれたのみ テストコミット

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 require dirname(__FILE__). '/__settings__.php';
 require dirname(__FILE__). '/__funcs__.php';
-app(); ?>
+app();
+?>
 <app name="Openpear" summary="PEAR Repository Channel and Subversion Hosting Service" ns="Openpear" unmatch_redirect="/">
 	<installation>
         # HatenaSyntaxを利用しているのでinstallします
