a7bb5fbb569bb109dd6fa51e6fade6bd19a1a87d
Keisuke SATO <riaf@nequal.jp>
2010-06-11 16:26:56 +0900

    ログインまわりの修正

--- libs/org/openpear/module/OpenpearAccountModule.php
+++ libs/org/openpear/module/OpenpearAccountModule.php
@@ -4,27 +4,27 @@ import('org.openpear.model.OpenpearMaintainer');
 class OpenpearAccountModule extends Object
 {
     public function login_condition(Request $request){
-        if($request->user() instanceof OpenpearMaintainer){
+        if ($request->user() instanceof OpenpearMaintainer) {
             return true;
         }
-        if($request->is_post()){
-            try{
+        if ($request->is_post() && $request->is_vars('login') && $request->is_vars('password')) {
+            try {
                 $user = C(OpenpearMaintainer)->find_get(
 		                    Q::ob(
 		                    	Q::b(Q::eq('name', $request->in_vars('login')))
-		                    	,Q::b(Q::eq('mail', $request->in_vars('mail')))
+		                    	,Q::b(Q::eq('mail', $request->in_vars('login')))
 		                    )
 		                );
-	            if(is_object($user)){
-	            	if($user->certify($request->in_vars('password'))){
+	            if ($user instanceof OpenpearMaintainer) {
+	            	if ($user->certify($request->in_vars('password'))) {
 			            $request->user($user);
 			            return true;
-		            }else{
+		            } else {
 		                Exceptions::add(new Exception('password is incorrect'), 'password');
 		            }
 	            }
-            } catch(Exception $e){
-                Exceptions::add($e, 'mail');
+            } catch (Exception $e) {
+                Log::debug($e);
             }
         }
         return false;
