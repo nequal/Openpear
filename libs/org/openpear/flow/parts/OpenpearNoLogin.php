ffd2e398d0024b4f1d63c00bc0ee93faa7c4f837
Keisuke SATO <riaf@nequal.jp>
2010-12-23 18:17:38 +0900

    OpenID の追加，プロフィールの編集実装 #230,231

--- libs/org/openpear/flow/parts/OpenpearNoLogin.php
+++ libs/org/openpear/flow/parts/OpenpearNoLogin.php
@@ -31,70 +31,23 @@ class OpenpearNoLogin extends Flow
      * @request string $openid_url openid認証サーバのURL
      */
     public function login_by_openid() {
-        Pea::begin_loose_syntax();
-        require_once 'Auth/OpenID/Consumer.php';
-        require_once 'Auth/OpenID/FileStore.php';
-        require_once 'Auth/OpenID/SReg.php';
-        require_once 'Auth/OpenID/PAPE.php';
-        $openid = null;
-
         if ($this->is_login()) $this->redirect_by_map('index');
-        if ((($this->in_vars('openid_url') != "") || $this->in_vars('openid_verify'))) {
-            Log::debug("begin openid auth: ". $this->in_vars('openid_url'));
 
-            // OpenID Auth
-            $consumer = new Auth_OpenID_Consumer(new Auth_OpenID_FileStore(work_path('openid')));
-            if ($this->is_vars('openid_verify')) {
-                $response = $consumer->complete($this->request_url());
-                if ($response->status == Auth_OpenID_SUCCESS) {
-                    $openid = $response->getDisplayIdentifier();
+        if ($openid = OpenpearOpenIDAuth::login($this)) {
+            try {
+                $openid_maintainer = C(OpenpearOpenidMaintainer)->find_get(Q::eq('url', $openid));
+                $this->user($openid_maintainer->maintainer());
+                if ($this->login()) {
+                    $redirect_to = $this->in_sessions('logined_redirect_to');
+                    $this->rm_sessions('logined_redirect_to');
+                    if(!empty($redirect_to)) $this->redirect($redirect_to);
+                    $this->redirect_by_map("login_redirect");
                 }
-            } else {
-                $auth_request = $consumer->begin($this->in_vars('openid_url'));
-                if (!$auth_request) {
-                    throw new RuntimeException('invalid openid url');
-                }
-                $sreg_request = Auth_OpenID_SRegRequest::build(array('nickname'), array('fullname', 'email'));
-                if ($sreg_request) {
-                    $auth_request->addExtension($sreg_request);
-                }
-                if ($auth_request->shouldSendRedirect()) {
-                    $redirect_url = $auth_request->redirectURL(url(), $this->request_url(false). '?openid_verify=true');
-                    if (Auth_OpenID::isFailure($redirect_url)) {
-                        throw new RuntimeException("Could not redirect to server: {$redirect_url->message}");
-                    } else {
-                        $this->redirect($redirect_url);
-                    }
-                } else {
-                    $form_html = $auth_request->htmlMarkup(url(),
-                        $this->request_url(false). '?openid_verify=true', false, array('id' => 'openid_message'));
-                    if (Auth_OpenID::isFailure($form_html)) {
-                        throw new RuntimeException("Could not redirect to server: {$form_html->message}");
-                    } else {
-                        echo $form_html;
-                        exit;
-                    }
-                }
-            }
-
-            Pea::end_loose_syntax();
-
-            if ($openid) {
-                try {
-                    $openid_maintainer = C(OpenpearOpenidMaintainer)->find_get(Q::eq('url', $openid));
-                    $this->user($openid_maintainer->maintainer());
-                    if ($this->login()) {
-                        $redirect_to = $this->in_sessions('logined_redirect_to');
-                        $this->rm_sessions('logined_redirect_to');
-                        if(!empty($redirect_to)) $this->redirect($redirect_to);
-                        $this->redirect_by_map("login_redirect");
-                    }
-                } catch (NotfoundDaoException $e) {
-                    $this->sessions('openid_identity', $openid);
-                    $this->redirect_by_map('signup');
-                } catch (Exception $e) {
-                    throw $e;
-                }
+            } catch (NotfoundDaoException $e) {
+                $this->sessions('openid_identity', $openid);
+                $this->redirect_by_map('signup');
+            } catch (Exception $e) {
+                throw $e;
             }
         }
         $this->do_login();
