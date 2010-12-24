16e865afc5c487a295474a9c334c240a39c7bb83
Keisuke SATO <riaf@nequal.jp>
2010-12-25 02:40:33 +0900

    fixed commands

--- bin/create_repos.php
+++ bin/create_repos.php
@@ -1,6 +1,5 @@
 <?php
 require_once dirname(__DIR__). '/__settings__.php';
-import('org.openpear.flow.parts.__init__');
 
 $queue = C(OpenpearNewprojectQueue)->find_get(Q::lt('trial_count', 5), Q::order('id'));
 $queue->create();
